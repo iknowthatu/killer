c9ef8b2064832a2d6d3a0478722c40da2bbb9494
deepdarkness <any@email.net>
2019-10-01 23:39:03 +0300

    refactor/little refactoring of TravellerHeart.js

--- src/Killer/TravellerHeart/TravellerHeart.js
+++ src/Killer/TravellerHeart/TravellerHeart.js
@@ -14,7 +14,7 @@ class TravellerHeart {
 
   nextPulse(params={}) {
     const newParams = {...params};
-    if(!params.needMove) {
+    if (!params.needMove) {
       newParams.nextLocationNumber = null;
       newParams.direction = 'fwd';
       newParams.waySource = '';
@@ -22,86 +22,87 @@ class TravellerHeart {
       newParams.destinationReached = false;
     }
 
-    //console.log(`is fight? ${params.isFight}`, `is Need Move? ${params.needMove}`,
-     // `isDestReached ${newParams.destinationReached}`);
-
-    if(params.isFight || !params.needMove || newParams.destinationReached) return newParams;
-
-    /*
-    if(params.onWrongPlaceCounter > 3) {
-      newParams.nextLocationNumber--;
-      newParams.prevLocationWays = null;
-      return newParams;
-    } */
-
-    //console.log(`place changed?: ${params.prevLocationWays != null && !this.isPlaceWasChanged(params.prevLocationWays)}`);
-    if(params.prevLocationWays != null && !this.isPlaceWasChanged(params.prevLocationWays)) {
-      //console.log(`place wasn't changed. prev:`, params.prevLocationWays, `counter: `, newParams.onWrongPlaceCounter);
-      //newParams.onWrongPlaceCounter = newParams.onWrongPlaceCounter && newParams.onWrongPlaceCounter + 1 || 1;
+    if (params.isFight || !params.needMove || newParams.destinationReached) {
       return newParams;
     }
 
-    //newParams.onWrongPlaceCounter = 0;
-    //console.log(`direction: ${newParams.direction}`);
+    if (params.prevLocationWays !== null && !this.isPlaceWasChanged(params.prevLocationWays)) {
+      return newParams;
+    }
 
     const wayChain = this.getWay(params.waySource, params.direction);
-    const locationNumber = params.nextLocationNumber != null ? params.nextLocationNumber : 0;
+    const locationNumber = params.nextLocationNumber !== null ? params.nextLocationNumber : 0;
 
-    if(locationNumber >= wayChain.length) {
+    if (locationNumber >= wayChain.length) {
       newParams.nextLocationNumber = 0;
       newParams.destinationReached = true;
       return newParams;
     }
 
-    //console.log(wayChain[locationNumber]);
     newParams.nextLocationNumber = locationNumber + 1;
     newParams.prevLocationWays = this.goToLocation(wayChain[locationNumber]);
+
     return newParams;
   }
 
   getWay(waytoheal, direction) {
-    if(!waytoheal) return 1; //no input value or empty
+    if (!waytoheal) {
+      return 1; //no input value or empty
+    }
     let way = direction == 'fwd' ? waytoheal.match(/([^]+?)\//) : waytoheal.match(/\/([^]+)/);
-    if(!way) return 2; // no slash in input value
-    way = way[1].split(';').map(_ => _.trim()).filter(_ => _);
+
+    if (!way) {
+      return 2; // no slash in input value
+    }
+    way = way[1].split(';').map(wayStep => wayStep.trim()).filter(wayStep => !!wayStep);
+
     return way;
   }
 
+  static getLocationButtons() {
+    return document.querySelectorAll('#divLocGo > .button');
+  }
+
   goToLocation(locationName) {
-    //console.log(locationName);
     let locationNumber = locationName.match(/\s+\d+/);
-    if(locationNumber) locationName = locationName.replace(/\s+\d+/, '');
-    locationNumber = locationNumber && !isNaN(+locationNumber[0]) && +locationNumber[0] || 0;
-    const locationButtons = document.querySelectorAll('#divLocGo > .button');
-    if(locationButtons.length < 1) {
+    if (locationNumber) {
+      locationName = locationName.replace(/\s+\d+/, '');
+    }
+
+    locationNumber = locationNumber && !isNaN(parseInt(locationNumber[0])) && parseInt(locationNumber[0]) || 0;
+    const locationButtonsNodes = TravellerHeart.getLocationButtons();
+    if (locationButtonsNodes.length < 1) {
       console.log('There is no exit');
       throw 'No exit here';
     }
 
-    const nextLocationButtons = Array.from(locationButtons)
-      .filter(locationButton => locationButton.innerHTML.match(new RegExp(locationName, 'i')));
+    const nextLocationButtons = Array.from(locationButtonsNodes)
+      .filter(locationButtonNode => locationButtonNode.innerHTML.match(new RegExp(locationName, 'i')));
 
-    if(nextLocationButtons.length < 1) {
+    if (nextLocationButtons.length < 1) {
       console.log(`no such place: ${locationName}`);
       throw `no such place: ${locationName}`;
     }
 
-    //console.log(`nextLoc: ${locationNumber}`, nextLocationButtons);
     const nextLocationButton = nextLocationButtons[locationNumber];
-    //console.log(`nextLocBtn:`, nextLocationButton);
     nextLocationButton.click();
 
-    return locationButtons;
+    return locationButtonsNodes;
   }
 
-  isPlaceWasChanged(oldLocationButtons) {
-    const locationButtons = document.querySelectorAll('#divLocGo > .button');
-    if(oldLocationButtons.length != locationButtons.length) return true;
+  isPlaceWasChanged(oldLocationButtonsNodes) {
+    const locationButtonsNodes = TravellerHeart.getLocationButtons();
+    if (oldLocationButtonsNodes.length !== locationButtonsNodes.length) {
+      return true;
+    }
 
-    for(let i = 0; i < oldLocationButtons.length; i++) {
-      const locationIdForOldButton = oldLocationButtons[0].outerHTML.match(/btnGo\d+/).toString();
-      const locationIdForNewButton = locationButtons[0].outerHTML.match(/btnGo\d+/).toString();
-      if(locationIdForNewButton != locationIdForOldButton) return true;
+    for (let i = 0, lenght = oldLocationButtonsNodes.length; i < length; i++) {
+      // @TODO seems this zero should be 'i' ?
+      const locationIdForOldButton = oldLocationButtonsNodes[0].outerHTML.match(/btnGo\d+/).toString();
+      const locationIdForNewButton = locationButtonsNodes[0].outerHTML.match(/btnGo\d+/).toString();
+      if (locationIdForNewButton !== locationIdForOldButton) {
+        return true;
+      }
     }
 
     return false;
